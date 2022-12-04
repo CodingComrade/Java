f7a4e7ac8777c6bd9babd56eb6e410d52561002e
Chris Small <chrissmall08.24@gmail.com>
2022-12-04 12:49:53 -0700

    challenge 5

--- Chapter6/Challenge 5/src/Main.java
+++ Chapter6/Challenge 5/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        int id = input.nextInt();
+        double pay = input.nextDouble();
+        int hours = input.nextInt();
+
+        Payroll chris = new Payroll(name, id);
+        chris.setHoursWorked(hours);
+        chris.setPayRate(pay);
+        System.out.println(chris.grossPay());
+
     }
 }
--- Chapter6/Challenge 5/src/Payroll.java
+++ Chapter6/Challenge 5/src/Payroll.java
@@ -1,3 +1,48 @@
 public class Payroll {
+    private String name;
+    private int id;
+    private double payRate;
+    private int hoursWorked;
+
+    public Payroll(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public double grossPay() {
+        return payRate * hoursWorked;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
 
 }
