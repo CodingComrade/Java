486386cbdde76c48440ab914ec6f3fe8acb57254
Chris Small <chrissmall08.24@gmail.com>
2022-12-09 15:50:13 -0700

    Chapter 10 Challenge 4

--- Chapter 10/Chapter 10 Challenge 4/src/Main.java
+++ Chapter 10/Chapter 10 Challenge 4/src/Main.java
@@ -4,8 +4,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int grade = input.nextInt();
+        int questions = input.nextInt();
+        int missed = input.nextInt();
         GradedActivity student = new GradedActivity();
         student.setScore(grade);
         System.out.println(student.getGrade());
+        FinalExam exam = new FinalExam(questions, missed);
+
+        System.out.println("each exam counts " + exam.getPointsEach() +
+                " points. \nThe exam score is " + exam.getScore() + "\nThe " +
+                "exam grade is " + exam.getGrade());
+
+
     }
 }
